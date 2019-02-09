b799ef173858b984033af13660bff04857fc7f01
logan42474 <loganmiller040402@gmail.com>
2019-02-08 22:37:37 -0700

    Basic program that gets the average score of our alliances last year

--- src/JSON_Parsing.java
+++ src/JSON_Parsing.java
@@ -1,9 +1,32 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class JSON_Parsing {
-public static void test(String input) {
-	JSONObject obj = new JSONObject(input);
-	int currentyear = obj.getInt("current_season");
-	System.out.println(currentyear);
+public static void AdverageScore(String input) {
+	double all = 0;
+	//input = "\"data\": "+input;
+	//System.out.println("Two: ----------"+input);
+	JSONArray arr = new JSONArray(input);
+	//JSONObject obj = new JSONObject(input);
+	
+	int output = arr.getJSONObject(0).getJSONObject("alliances").getJSONObject("red").getInt("score");
+	for(int i = 0; i< arr.length();i++) {
+		boolean notfound = true;
+		for(int ie = 0;ie<3;ie++) {
+			if(arr.getJSONObject(i).getJSONObject("alliances").getJSONObject("red").getJSONArray("team_keys").getString(ie).equals("frc2486")) {
+				System.out.println("Red");
+				all+=arr.getJSONObject(i).getJSONObject("alliances").getJSONObject("red").getDouble("score");
+			}
+			if(arr.getJSONObject(i).getJSONObject("alliances").getJSONObject("blue").getJSONArray("team_keys").getString(ie).equals("frc2486")) {
+				System.out.println("Blue");
+				all+=arr.getJSONObject(i).getJSONObject("alliances").getJSONObject("blue").getDouble("score");
+			}
+		}
+		//if(arr.getJSONObject(i).getJSONObject("alliances").getJSONObject("red").getJSONArray("team_keys").)
+	}
+	System.out.println(all);
+	System.out.println(all/arr.length());
+	//System.out.println(arr.length());
+	//System.out.println(output);
 }
 }
--- src/Run.java
+++ src/Run.java
@@ -3,8 +3,9 @@ public class Run {
 	//2019cave Ventura
 	//team/frc2486/matches/2019
 	public static void main(String[] args) throws Exception {
-		System.out.println(HTTP.APIRead("status"));
-		JSON_Parsing.test(HTTP.APIRead("status"));
+		String command = "team/frc2486/matches/2018/simple";
+		System.out.println(HTTP.APIRead(command));
+		JSON_Parsing.AdverageScore(HTTP.APIRead(command));
 	}
 
 }
