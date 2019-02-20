2242364358f37c6f895576cbb180934ff67036a4
logan42474 <loganmiller040402@gmail.com>
2019-02-19 18:07:21 -0700

    Write Data to CSV
    Application now Takes all data needed from TBA and puts it in the MatchData hashmap

--- src/CSV/CSVWriter.java
+++ src/CSV/CSVWriter.java
@@ -14,20 +14,19 @@ import Data.GameData;
 public class CSVWriter {
 public static void WriteGameData(String filepath) throws IOException {
 	 BufferedWriter writer = Files.newBufferedWriter(Paths.get(filepath));
-	CSVPrinter csv = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("TeamKey", "Alliance"));
+	CSVPrinter csv = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("TeamKey", "Match Number"));
 	
 	for(int i = 0;i<GameData.matchdata.size();i++) {
 		for(int ie = 0;ie<GameData.matchdata.get(GameData.teamkeys.get(i)).size();ie++) {
-			csv.printRecord(GameData.teamkeys.get(i),GameData.matchdata.get(GameData.teamkeys.get(i)).get(ie).alliance);
+			//GameData.matchdata.get(GameData.teamkeys.get(i)).get(ie)
+			if(GameData.matchdata.get(GameData.teamkeys.get(i)).get(ie).alliance.equals("blue")) {
+			csv.printRecord(GameData.teamkeys.get(i),GameData.matchdata.get(GameData.teamkeys.get(i)).get(ie).matchNumber);
+			}
+			if(GameData.matchdata.get(GameData.teamkeys.get(i)).get(ie).alliance.equals("red")) {
+				
+			}
 		}
 	}
-	/*
-	System.out.println(GameData.teamkeys.get(0));
-	System.out.println("WUT: "+GameData.matchdata.get(GameData.teamkeys.get(0)));
-	csv.printRecord(GameData.matchdata.get(GameData.teamkeys.get(0)).get(0).alliance);
-	//GameData.matchdata.get(GameData.teamkeys.get(0)).get(0).alliance
-	csv.printRecord("test?","idk whats happening");
-	*/
 	csv.flush();
 }
 }
